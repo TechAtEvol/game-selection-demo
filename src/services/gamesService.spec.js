fb66ccad9326b0bbe08570201ecfe43f79558ab1
Staffan Frisk <tech@evol.se>
2019-05-21 22:49:34 +0200

    Caught a test that failed after a renamed method

--- src/services/gamesService.spec.js
+++ src/services/gamesService.spec.js
@@ -4,7 +4,7 @@ describe('gamesService', () => {
   const gamesService = createGamesService();
   describe('when calling getAll', () => {
     it('it returns a list of games', () => {
-      const allGames = gamesService.getAll()
+      const allGames = gamesService.getAllGames()
       expect(allGames.length > 0).toBe(true)
     })
   })
