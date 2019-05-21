305ad5deb5b6e10e611c8c992b6a7dfecb61a6be
Staffan Frisk <tech@evol.se>
2019-05-21 10:09:42 +0200

    added some todos

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import GameGrid from './components/GameGrid';
 import createGameService from './services/gamesService'
 
 function App() {
+  // TODO: This could be nice to have in GameGrid instead probably
   const gamesService = createGameService();
   const games = gamesService.getAll();
+  // TODO: gameProviders, gameCollectionIDs could be functions of the service and calculated from the gamelist
   const gameProviders = [
     {value:"All",label:"All"},
     {value:"OGS",label:"OGS"},
@@ -15,6 +17,7 @@ function App() {
     {value:"slots",label:"slots"},
   ];
 
+  // TODO: Support branding
   return (
     <div className="App">
       <h3 className="ui block header">My Games</h3>
